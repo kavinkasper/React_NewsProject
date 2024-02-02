c227504e54b0df58fffc388767ace803f5124b67
kavinkasper <142666281+kavinkasper@users.noreply.github.com>
2024-02-02 11:13:47 +0530

    Update News.js

--- src/News.js
+++ src/News.js
@@ -3,54 +3,60 @@ import React from "react";
 import { useState, useEffect } from "react";
 import apiConfig from "./Apiconfig";
 import "./News.css";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const News = () => {
   const [value, setValue] = useState([]);
-  const REQ_ULR =
-    `https://newsapi.org/v2/everything?q=apple&from=2023-12-27&to=2023-12-27&sortBy=popularity&apiKey=${apiConfig.apiKey}`;
+  const REQ_ULR = `https://newsapi.org/v2/everything?q=bitcoin&apiKey=${apiConfig.apiKey}`;
 
-     // Fetching news data's when the component is Loaded
+  // Fetching news data's when the component is Loaded
   useEffect(() => {
     const fetchNewsData = async () => {
       try {
         const response = await axios.get(REQ_ULR);
         let result = await response.data;
-        let p = result.articles.map((article) => {
-            //Filtering out articles with missing or false properties
-          if (article.title && article.urlToImage && article.description) {
-            return (
-              <div key={article.title} className="news-card">
-                <img
-                  className="card-image"
-                  src={article.urlToImage}
-                  alt="News images"
-                />
-                <div className="card-content">
-                  <div className="card-title">{article.title}</div>
-                  {article.author && (
-                    <p className="author-info">-by {article.author}</p>
-                  )}
-                  <p className="card-description">{article.description}</p>
-                  <a href={article.url} className="read-more-link">
-                    Read more...
-                  </a>
-                </div>
-              </div>
-            );
-          }
-          return null;
-        });
-        setValue(p);
-
-      } catch (error) {
-        console.log("Error occured is:", error);
+        setValue(result.articles);
+        console.log("Hii");
+      } catch (err) {
+        alert("Error: " + err);
       }
     };
-
     fetchNewsData();
-  },[]);      //The empty dependency array ensures that useEffect runs only once when the component is loaded
+  }, [REQ_ULR]);
+  //The dependency array ensures that useEffect runs only when the REQ_URL is called.
 
-  return <div className="news-container">{value}</div>;
+  const validArticles = value.filter(
+    (article) => article.title && article.urlToImage && article.description
+  );
+
+  return (
+    <React.Fragment>
+      <Header />
+      <div className="news-container">
+        {validArticles.map((article) => (
+          <div key={article.title} className="news-card">
+            <img
+              className="card-image"
+              src={article.urlToImage}
+              alt="News images"
+            />
+            <div className="card-content">
+              <div className="card-title">{article.title}</div>
+              {article.author && (
+                <p className="author-info">-by {article.author}</p>
+              )}
+              <p className="card-description">{article.description}</p>
+              <a href={article.url} className="read-more-link">
+                Read more...
+              </a>
+            </div>
+          </div>
+        ))}
+      </div>
+      <Footer />
+    </React.Fragment>
+  );
 };
 
 export default News;
